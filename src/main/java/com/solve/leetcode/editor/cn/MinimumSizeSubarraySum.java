453957d666810a73749a3d6efbd99c16c64119fc
wuou <wuou@tal.com>
2021-04-07 11:04:30 +0800

    P209 --- 长度最小的子数组

---
+++ src/main/java/com/solve/leetcode/editor/cn/MinimumSizeSubarraySum.java
@@ -0,0 +1,88 @@
+package com.solve.leetcode.editor.cn;
+
+//给定一个含有 n 个正整数的数组和一个正整数 target 。 
+//
+// 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长
+//度。如果不存在符合条件的子数组，返回 0 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：target = 7, nums = [2,3,1,2,4,3]
+//输出：2
+//解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：target = 4, nums = [1,4,4]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= target <= 109 
+// 1 <= nums.length <= 105 
+// 1 <= nums[i] <= 105 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。 
+// 
+// Related Topics 数组 双指针 二分查找 
+// 👍 599 👎 0
+
+
+/**
+ * P209 --- 长度最小的子数组
+ **/
+public class MinimumSizeSubarraySum {
+    public static void main(String[] args) {
+        Solution solution = new MinimumSizeSubarraySum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+            // 暴力破解
+            int sum = 0;
+            int result = 0;
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum = 0;
+                for (int j =i; j < nums.length; j++) {
+                    sum += nums[j];
+
+                    index = j - i + 1;
+
+                    if (sum >= target && result == 0) {
+                        result = index;
+                    } else if (sum >= target && result != 0) {
+                        result = result < index ? result : index;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
