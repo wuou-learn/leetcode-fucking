956f3f97228abf23783481e786b8de79c7cc6f9d
wuou <wuou@tal.com>
2021-08-18 10:02:53 +0800

    92-反转链表

--- src/main/java/com/solve/单链表/LeetCode92.java
+++ src/main/java/com/solve/单链表/LeetCode92.java
@@ -7,31 +7,24 @@ package com.solve.单链表;
  * @Version 1.0.0
  */
 public class LeetCode92 {
+
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head.next == null){
+        if(left == 1){
+            return revereRight(head,right);
+        }
+        head.next = reverseBetween(head.next,left-1,right-1);
+        return head;
+    }
+
+    ListNode successor = null;
+    private ListNode revereRight(ListNode head, int right) {
+        if(right == 1){
+            successor = head.next;
             return head;
         }
-        return null;
-    }
-
-    static ListNode reverse(ListNode head) {
-        if (head.next == null) return head;
-        ListNode last = reverse(head.next);
+        ListNode last = revereRight(head.next, right - 1);
         head.next.next = head;
-        head.next = null;
+        head.next = successor;
         return last;
-    }
-
-    public static void main(String[] args) {
-        ListNode [] listNodes = new ListNode[5];
-        listNodes[0] = new ListNode(1,listNodes[1]);
-        listNodes[1] = new ListNode(7,listNodes[2]);
-        listNodes[2] = new ListNode(9,listNodes[3]);
-        listNodes[3] = new ListNode(10,listNodes[4]);
-        listNodes[4] = new ListNode(20,null);
-
-        for(int i =0; i<5; i++){
-            reverse(listNodes[i]);
-        }
     }
 }
