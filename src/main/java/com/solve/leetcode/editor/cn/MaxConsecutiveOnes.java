2dace454aefc21cb0d35052e9660692425d19fc0
wuou <1521658453@qq.com>
2022-06-23 19:37:10 +0800

    feat: 285 add

---
+++ src/main/java/com/solve/leetcode/editor/cn/MaxConsecutiveOnes.java
@@ -0,0 +1,64 @@
+//给定一个二进制数组 nums ， 计算其中最大连续 1 的个数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,0,1,1,1]
+//输出：3
+//解释：开头的两位和最后的三位都是连续 1 ，所以最大连续 1 的个数是 3.
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：nums = [1,0,1,1,0,1]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// nums[i] 不是 0 就是 1. 
+// 
+// Related Topics数组 
+//
+// 👍 323, 👎 0 
+//
+//
+//
+//
+
+
+package com.solve.leetcode.editor.cn;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        Solution solution = new MaxConsecutiveOnes().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int findMaxConsecutiveOnes(int[] nums) {
+            // 二进制数组nums
+            // 最大连续的数
+            int max = 0, count =0;
+            for (int i =0; i < nums.length; i++) {
+                if (nums[i] == 1) {
+                    count++;
+                } else {
+                    max = Math.max(max, count);
+                    count = 0;
+                }
+            }
+            return Math.max(max, count);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
