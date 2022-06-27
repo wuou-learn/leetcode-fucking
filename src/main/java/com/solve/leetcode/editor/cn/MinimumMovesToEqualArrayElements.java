d5f13d7da533a129bb26e86cc1bfb9aeb5eaec9e
wuou <1521658453@qq.com>
2022-06-27 14:57:57 +0800

    feat: 453 add

--- src/main/java/com/solve/leetcode/editor/cn/MinimumMovesToEqualArrayElements.java
+++ src/main/java/com/solve/leetcode/editor/cn/MinimumMovesToEqualArrayElements.java
@@ -52,7 +52,7 @@ public class MinimumMovesToEqualArrayElements {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minMoves(int[] nums) {
-            int min = Integer.MAX_VALUE;
+            int min = nums[0];
             int sum = 0;
             int n = nums.length;
             for (int i =0; i<n; i++) {
