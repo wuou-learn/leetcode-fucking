ce3d58d979837d17f4c3bb4d23006e946013fae7
wuou <1521658453@qq.com>
2022-07-19 15:27:23 +0800

    feat: 反转字符串

---
+++ src/main/java/com/solve/leetcode/editor/cn/ReverseString.java
@@ -0,0 +1,63 @@
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = ["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s[i] 都是 ASCII 码表中的可打印字符 
+// 
+// Related Topics递归 | 双指针 | 字符串 
+//
+// 👍 621, 👎 0 
+//
+//
+//
+//
+
+
+package com.solve.leetcode.editor.cn;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Solution solution = new ReverseString().new Solution();
+        String hello = new String("hello");
+        solution.reverseString(hello.toCharArray());
+    }
+
+    /**
+     * 反转字符串
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void reverseString(char[] s) {
+
+            for (int i =0, j=s.length-1; i < s.length && j >= i; i++,j--) {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
