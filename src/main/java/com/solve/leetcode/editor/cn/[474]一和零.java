a399d12dfa47e4de4fa389071bf13e0dddb2808f
wuou <1521658453@qq.com>
2022-08-04 14:29:21 +0800

    feat: 一和零

---
+++ src/main/java/com/solve/leetcode/editor/cn/[474]一和零.java
@@ -0,0 +1,43 @@
+package com.solve.leetcode.editor.cn;
+
+class OnesAndZeroes {
+    public static void main(String[] args) {
+        Solution solution = new OnesAndZeroes().new Solution();
+    }
+
+    /**
+     * 一和零
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findMaxForm(String[] strs, int m, int n) {
+            int len = strs.length;
+            int[][][] dp = new int[len + 1][m + 1][n + 1];
+
+            for (int i = 1; i <= len; i++) {
+                int[] count = count(strs[i-1]);
+                for (int j = 0; j <= m; j++) {
+                    for (int k = 0; k <= n; k++) {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                        int zero = count[0];
+                        int one = count[1];
+                        if (j >= zero && k >= one) {
+                            dp[i][j][k] = Math.max(dp[i-1][j][k], dp[i-1][j-zero][k-one]+1);
+                        }
+                    }
+                }
+            }
+            return dp[len][m][n];
+        }
+
+        public int[] count(String str) {
+            int[] cnt = new int[2];
+            for (char c : str.toCharArray()) {
+                cnt[c - '0']++;
+            }
+            return cnt;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
