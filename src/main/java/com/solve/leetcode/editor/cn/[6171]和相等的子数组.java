38bbd2b14c84ba02620faa4378d5a74bf15c308f
wuou <1521658453@qq.com>
2022-09-04 13:00:42 +0800

    feat: 和相等的子数组

--- src/main/java/com/solve/leetcode/editor/cn/[6171]和相等的子数组.java
+++ src/main/java/com/solve/leetcode/editor/cn/[6171]和相等的子数组.java
@@ -1,8 +1,13 @@
 package com.solve.leetcode.editor.cn;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 class FindSubarraysWithEqualSum {
     public static void main(String[] args) {
         Solution solution = new FindSubarraysWithEqualSum().new Solution();
+        solution.
+                findSubarrays(new int[]{4,2,4});
     }
 
     /**
@@ -11,7 +16,17 @@ class FindSubarraysWithEqualSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean findSubarrays(int[] nums) {
+            if (nums.length == 2) {
+                return false;
+            }
+            HashSet<Integer> set = new HashSet<>();
+            for (int i = 1; i < nums.length; i++) {
+                if (!set.add(nums[i-1] + nums[i])) {
+                    return true;
+                }
+            }
 
+            return false;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
