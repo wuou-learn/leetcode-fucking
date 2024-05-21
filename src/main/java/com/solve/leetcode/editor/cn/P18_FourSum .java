64bedc430ecd354fc15629259f869bada742314f
wuou <1521658453@qq.com>
2024-05-21 21:07:35 +0800

    feat: [18] 四数之和

---
+++ src/main/java/com/solve/leetcode/editor/cn/P18_FourSum .java
@@ -0,0 +1,97 @@
+//给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[
+//b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）： 
+//
+// 
+// 0 <= a, b, c, d < n 
+// a、b、c 和 d 互不相同 
+// nums[a] + nums[b] + nums[c] + nums[d] == target 
+// 
+//
+// 你可以按 任意顺序 返回答案 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,0,-1,0,-2,2], target = 0
+//输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,2,2,2,2], target = 8
+//输出：[[2,2,2,2]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 1916 👎 0
+
+
+package com.solve.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 四数之和
+ * @author wuou
+ * @date 2024-05-19 20:45:41
+ */
+class P18_FourSum{
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P18_FourSum().new Solution();
+        solution.fourSum(new int[]{1000000000,1000000000,1000000000,1000000000}, -294967296);
+    }
+
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            // i 去重
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                // j 去重
+                if (j > i+1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+                for (int k = j + 1, l = nums.length - 1; k < l; k++) {
+                    if (k > j + 1 && nums[k] == nums[k-1]) {
+                        continue;
+                    }
+                    // k < l - 1 避免指针k与l 重复
+                    while (k < l - 1 && nums[i] + nums[j] + nums[k] + nums[l-1] >= target) l--;
+                    if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+                        res.add(list);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
