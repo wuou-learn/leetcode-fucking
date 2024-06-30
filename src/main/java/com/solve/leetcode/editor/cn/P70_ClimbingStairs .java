e870c2acd9d92198ad1d039c0aff8d210b41de8e
wuou <1521658453@qq.com>
2024-06-30 20:19:10 +0800

    feat: 70

--- src/main/java/com/solve/leetcode/editor/cn/P70_ClimbingStairs .java
+++ src/main/java/com/solve/leetcode/editor/cn/P70_ClimbingStairs .java
@@ -108,14 +108,11 @@ class Solution {
         return m[n];
     }*/
     public int climbStairs(int n) {
-        if (n == 1 || n == 2) {
-            return n;
-        }
-        int a = 1, b = 2;
-        for (int i = 3; i <= n; i++) {
-            int temp = b;
-            b = a + b;
-            a = temp;
+        int a = 1, b = 1;
+        while (-- n > 0) {
+            int c = a + b;
+            a = b;
+            b = c;
         }
         return b;
     }
